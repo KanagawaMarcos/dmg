1851cd383406ecfdc1630a88b62f9d3439355461
Marcos Costa <mcs1901@outlook.com>
2021-05-08 21:17:51 +0000

    fix: workaround for certificates

--- GrpcClientCSharp/Program.cs
+++ GrpcClientCSharp/Program.cs
@@ -9,7 +9,12 @@ namespace GrpcClientCSharp
     {
         static async Task Main(string[] arg)
         {
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var httpHandler = new HttpClientHandler();
+            // @TODO figure out how to handle certificates properly with GRPC
+            httpHandler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001",
+                new GrpcChannelOptions { HttpHandler = httpHandler });
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                 new HelloRequest { Name = "GreeterClient" }
