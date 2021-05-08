7a7df3dca32f3e2c0df88f3698aa8a0e534a6dcd
Marcos Costa <mcs1901@outlook.com>
2021-05-08 21:59:20 +0000

    chore: unify projects under a solution

---
+++ db/Library.fs
@@ -0,0 +1,7 @@
+﻿module Library
+
+open Newtonsoft.Json
+
+let getJsonNetJson value =
+    let json = JsonConvert.SerializeObject(value)
+    $"I used to be {value} but now I'm {json} thanks to JSON.NET!"
