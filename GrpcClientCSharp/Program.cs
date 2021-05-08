25d6322ca3b62a2f171bc85c49639b9798f8df25
Marcos Costa <mcs1901@outlook.com>
2021-05-08 20:16:05 +0000

    feat: add clientside example

---
+++ GrpcClientCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace GrpcClientCSharp
+{
+    class Program
+    {
+        static async Task Main(string[] arg)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" }
+            );
+
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
