645574a0505897c2d05de2265d142ff8058fd448
Marcos Costa <marcos.costa@datarisk.io>
2021-05-15 16:39:28 -0300

    feat: insert into backend

--- GrpcRouter/Services/InspectorService.cs
+++ GrpcRouter/Services/InspectorService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
+using Repository;
 
 namespace GrpcRouter
 {
@@ -17,6 +18,14 @@ namespace GrpcRouter
 
         public override Task<Reply> SaveInspection(InspectionRequest inspection, ServerCallContext context)
         {
+            try
+            {
+                Database.saveInspection(inspection.Retailer);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
             return Task.FromResult(new Reply
             {
                 Message = "The retailer is " + inspection.Retailer
--- InspectionServer/Repository.fs
+++ InspectionServer/Repository.fs
@@ -9,7 +9,7 @@ type InspectionDTO = {
     rooftop_rate: int
 }
 
-module database =
+module Database =
     
     [<Literal>]
     let connection =
@@ -17,11 +17,11 @@ module database =
 
     type Postgres = NpgsqlConnection<connection>
 
-    let saveInspection inspection = 
+    let saveInspection retailer = 
         use cmd = Postgres.CreateCommand<"
             INSERT INTO inspections 
                 (retail, lighting_rate, snow_rate, rooftop_rate)
             VALUES
                 (@retail, @lighting_rate,@snow_rate,@rooftop_rate)">(connection)
-        cmd.Execute(retail=inspection, lighting_rate= 1, snow_rate=2 ,rooftop_rate=3)
+        cmd.Execute(retail=retailer, lighting_rate= 1, snow_rate=2 ,rooftop_rate=3)
 
