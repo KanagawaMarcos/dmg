8f6b43ccecf38cb75331d26de1e4116a6e04504b
Marcos Costa <mcs1901@outlook.com>
2021-05-15 19:17:03 -0300

    chore: add ci (#7)
    * Create dotnet.yml
    
    * chore: add readme
    
    * chore: add postgres
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci port
    
    * fix: ci
    
    * fix: ports
    
    * fix: ports
    
    * fix: remove options
    
    * chore: lint code
    
    * fix: ci

--- InspectionClient/Program.fs
+++ InspectionClient/Program.fs
@@ -6,16 +6,27 @@ open System.Net.Http
 [<EntryPoint>]
 let main argv =
     let httpHandler = new HttpClientHandler()
-    httpHandler.ServerCertificateCustomValidationCallback <- 
-        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+    httpHandler.ServerCertificateCustomValidationCallback <-
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+
     let options = new GrpcChannelOptions()
     options.HttpHandler <- httpHandler
-    
-    let channel = GrpcChannel.ForAddress("https://localhost:5001", options)
+
+    let channel =
+        GrpcChannel.ForAddress("https://localhost:5001", options)
+
     let client = new Inspector.InspectorClient(channel)
 
-    let inspection = new InspectionRequest(Retailer="Le Biscuit",SnowRate=InspectionRequest.Types.Rate.Bad, RooftopRate=InspectionRequest.Types.Rate.Average,LightingRate=InspectionRequest.Types.Rate.Good)
+    let inspection =
+        new InspectionRequest(
+            Retailer = "Le Biscuit",
+            SnowRate = InspectionRequest.Types.Rate.Bad,
+            RooftopRate = InspectionRequest.Types.Rate.Average,
+            LightingRate = InspectionRequest.Types.Rate.Good
+        )
+
     let reply = client.SaveInspection(inspection)
-    
+
     printfn "%s" reply.Message
     0
--- InspectionServer/Repository.fs
+++ InspectionServer/Repository.fs
@@ -2,34 +2,40 @@
 
 open FSharp.Data.Npgsql
 
-type InspectionDTO = {
-    retail: string
-    lighting_rate: int
-    snow_rate: int
-    rooftop_rate: int
-}
-module Dtos = 
-    let toInspectionDto retailer snow lighting rooftop  = 
-        {
-            retail=retailer
-            snow_rate=snow
-            lighting_rate=lighting
-            rooftop_rate=rooftop
-        }
+type InspectionDTO =
+    { retail: string
+      lighting_rate: int
+      snow_rate: int
+      rooftop_rate: int }
+
+module Dtos =
+    let toInspectionDto retailer snow lighting rooftop =
+        { retail = retailer
+          snow_rate = snow
+          lighting_rate = lighting
+          rooftop_rate = rooftop }
 
 module Database =
-    
+
     [<Literal>]
     let connection =
         "Host=localhost;Database=dmg;Username=dmg;Password=dmg"
 
     type Postgres = NpgsqlConnection<connection>
 
-    let saveInspection inspection = 
-        use cmd = Postgres.CreateCommand<"
-            INSERT INTO inspections 
+    let saveInspection inspection =
+        use cmd =
+            Postgres.CreateCommand<"
+            INSERT INTO inspections
                 (retail, lighting_rate, snow_rate, rooftop_rate)
             VALUES
-                (@retail, @lighting_rate,@snow_rate,@rooftop_rate)">(connection)
-        cmd.Execute(retail=inspection.retail, lighting_rate=inspection.lighting_rate, snow_rate=inspection.snow_rate ,rooftop_rate=inspection.rooftop_rate)
+                (@retail, @lighting_rate,@snow_rate,@rooftop_rate)">(
+                connection
+            )
 
+        cmd.Execute(
+            retail = inspection.retail,
+            lighting_rate = inspection.lighting_rate,
+            snow_rate = inspection.snow_rate,
+            rooftop_rate = inspection.rooftop_rate
+        )
